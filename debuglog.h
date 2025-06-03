ee536ee9cac347587eac88b33dc7600fd954f7df
Anoop Kumar Narayanan <anoop.kn@live.in>
2025-06-03 23:57:33 +0530

    Add files via upload

---
+++ debuglog.h
@@ -0,0 +1,27 @@
+#ifndef DEBUGLOG_H_INCLUDED
+#define DEBUGLOG_H_INCLUDED
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void debuglog(char *level, string info ){
+    switch(level[0]){
+    case 'D':
+        break;
+    case 'I':
+        break;
+    case 'W':
+        break;
+    case 'E':
+        break;
+    case 'X':
+        break;
+    case ' ':
+        break;
+    default:
+        break;
+    }
+}
+
+#endif // DEBUGLOG_H_INCLUDED
