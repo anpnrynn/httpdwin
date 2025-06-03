7a548b8bfde0a9e44cd4cf993685d5071488f5cf
Anoop Kumar Narayanan <anoop.kn@live.in>
2025-06-04 01:31:35 +0530

    Add files via upload

---
+++ threadpool.h
@@ -0,0 +1,23 @@
+#ifndef THREADPOOL_H_INCLUDED
+#define THREADPOOL_H_INCLUDED
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+using namespace std;
+
+class ThreadPool{
+public :
+
+    ThreadPool();
+    ~ThreadPool();
+
+    void threadPoolFunction(int threadid, SOCKET fd){
+    }
+
+    bool assignTask ();
+};
+
+
+#endif // THREADPOOL_H_INCLUDED
